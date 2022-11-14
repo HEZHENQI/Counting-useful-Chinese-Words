7d71d4d5f8b19177fa8a6a82354b30d68975fdab
HEZHENQI <72725463+HEZHENQI@users.noreply.github.com>
2022-11-14 16:21:28 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -5,4 +5,4 @@ export STOP_WORDS_PATH=""
 export CSV_PATH=""
 export SAVE_PATH=""
 
-python $PYTHON_FILE --stop_words_path=$STOP_WORDS_PATH --CSV_PATH=$CSV_PATH --SAVE_PATH=$SAVE_PATH
+python $PYTHON_FILE --stop_words_path=$STOP_WORDS_PATH --csv_path=$CSV_PATH --save_path=$SAVE_PATH
